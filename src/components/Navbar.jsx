2b5319aba32b97700a69b2f49f3bca0e7152b9bb
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-02-18 15:57:07 +0100

    styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,41 +6,43 @@ import { useContext } from "react";
 const Navbar = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
-  let lightURLTheme = "https://www.iconbolt.com/iconsets/fluent-emoji-flat/sun.svg";
-  let darkURLTheme ="https://www.iconbolt.com/iconsets/fluent-emoji-flat/crescent-moon.svg";
+  let lightURLTheme =
+    "https://www.iconbolt.com/iconsets/fluent-emoji-flat/sun.svg";
+  let darkURLTheme =
+    "https://www.iconbolt.com/iconsets/fluent-emoji-flat/crescent-moon.svg";
 
   return (
     <div className={`navbar ${theme}`}>
       <div className="navbar-container">
-        <div className="logo-container">
+        <div className="navbar-topside">
           <Link to="/home" className="logo">
             <img
-              src="https://www.iconbolt.com/iconsets/pixelicons-line/soccer.svg"
+              src="https://www.iconbolt.com/iconsets/hawcons-filled/soccer-court.svg"
               alt="football-logo"
             />
           </Link>
-          <p className="logo-text">Sve za Fudbal</p>
+          <p className="logo-text">Sve za Fudbal.com</p>
+          <div className="navbar-buttons">
+            <Language />
+            <button onClick={toggleTheme} className="theme-button">
+              {theme === "light" ? (
+                <img src={lightURLTheme} alt="Sun" />
+              ) : (
+                <img src={darkURLTheme} alt="Moon" />
+              )}
+            </button>
+          </div>
         </div>
-        <div className="navlinks">
-          <NavLink to="/home">Početna</NavLink>
-          <NavLink to="/about_us">O Nama</NavLink>
-          <NavLink to="/find_club">Pronađi klub</NavLink>
-          {/* <NavLink to="/you_play">Odigraj termin</NavLink>
-          <NavLink to="/leagues">Lige</NavLink>
-          <NavLink to="/tournaments">Turniri</NavLink>
-          <NavLink to="/camps">Kampovi</NavLink>
-          <NavLink to="/individual">Individualni Rad</NavLink> */}
-        </div>
-        <div className="navbar-buttons">
-          <Language />
-          <button onClick={toggleTheme} className="theme-button" >
-            {theme === "light" ? (
-              <img src={lightURLTheme} alt="Sun" />
-            ) : (
-              <img src={darkURLTheme} alt="Moon" />
-            )}
-          </button>
-        </div>
+          <div className="navlinks">
+            <NavLink to="/home">Početna</NavLink>
+            <NavLink to="/about_us">O Nama</NavLink>
+            <NavLink to="/find_club">Pronađi klub</NavLink>
+            <NavLink to="/you_play">Odigraj termin</NavLink>
+            <NavLink to="/leagues">Lige</NavLink>
+            <NavLink to="/tournaments">Turniri</NavLink>
+            <NavLink to="/camps">Kampovi</NavLink>
+            <NavLink to="/individual">Individualni Rad</NavLink>
+          </div>
       </div>
     </div>
   );
